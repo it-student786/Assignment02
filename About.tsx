201333e151d9b9f4722565dd77d9e9d25ca0034c
it-student786 <ansarhussainlashari125@gmail.com>
2024-10-09 12:14:16 +0500

    Add files via upload

---
+++ About.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <h1>Welcome to About section</h1>
+    );
+};
+
+export default About;
---
+++ page.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from "./components/Header";
+import About from "./components/About";
+
+
+const HomePage = () => {
+  return (
+    <div>
+      <Header/>
+      <About/>
+    </div>
+  );
+};
+
+export default HomePage;
+
+
+
+
